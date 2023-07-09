4f7d32a0c312f125294a45116b1e3d3c65511af9
Augusto <augustoscher@gmail.com>
2023-07-09 15:54:54 -0300

    feat: get count of integer results of math square of range

--- js/sherlock-squares.js
+++ js/sherlock-squares.js
@@ -1,11 +1,16 @@
 // Complete the squares function in the editor below. It should return an integer representing the number of square integers in the inclusive range from  to .
 
-const fillRange = (start, end) => Array(end - start + 1).fill().map((_item, index) => start + index);
-
-const getSquares = (start, end) => {
-  const range = fillRange(start, end);
-  return range.filter((num) => Number.isInteger(Math.sqrt(num))).length;
+function squares(a, b) {
+  const newA = Math.sqrt(a) % 1 === 0 ? a - 1: a;
+  const newB = Math.sqrt(b) % 1 === 0 ? b + 1: b;
+  // console.log(newA, newB)
+  // console.log(Math.floor(Math.sqrt(newB)))
+  // console.log(Math.floor(Math.sqrt(newA)))
+  return (Math.floor(Math.sqrt(newB)) - Math.floor(Math.sqrt(newA)));
 }
 
-console.log(getSquares(25, 49)) //2
-console.log(getSquares(17, 24)) //0
+
+console.log(squares(24, 49)) //3
+console.log(squares(59, 999999922) === 31615)
+console.log(squares(9, 999999966) === 31620)
+console.log(squares(23, 999999988) === 31618) 
